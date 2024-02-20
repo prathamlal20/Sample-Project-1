2a36460f5c7ff0901b6b543513fdd0e9b290c644
prathamlal20 <71704137+prathamlal20@users.noreply.github.com>
2024-02-20 11:52:05 +0530

    Initial Version

---
+++ src/main/java/com/octobit8/Sample/Project1/SampleProject1Application.java
@@ -0,0 +1,13 @@
+package com.octobit8.Sample.Project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/octobit8/Sample/Project1/SampleProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octobit8.Sample.Project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
